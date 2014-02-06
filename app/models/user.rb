d2d73ac8b874f712febbbcada648d80de1f60065
obliviusm <obliviusm@gmail.com>
2014-02-06 22:40:05 +0200

    unvalidatable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :validatable
+  devise :database_authenticatable
 
   validates_presence_of   :login
   validates_uniqueness_of :login
