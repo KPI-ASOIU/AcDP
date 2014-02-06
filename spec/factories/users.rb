9f9b551b7bab61de4c7356bba8f252c3bb3997d4
Dmytro Yarmak <dyarmak@heliostech.hk>
2014-02-06 12:17:57 +0200

    Fill factories with data

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,27 @@
 
 FactoryGirl.define do
   factory :user do
+    sequence(:login) do |n|
+     "user#{n}"
+    end
+
+    password { |u| "#{u.login}-password" }
+    email { |u| "#{u.login}@asoui.fiot.kpi.ua"}
   end
 
   factory :user_admin, parent: :user do
+    sequence(:login) do |n|
+     "admin#{n}"
+    end
     # TODO: set role = admin
+  end
+
+  factory :user_student, parent: :user do
+    sequence(:login) do |n|
+      student_number = (n % 30).to_s.rjust(2, "0")
+      group_number = ((n/30) % 100).to_s.rjust(2, "0")
+      group_letter = ("s".ord + n/3000).chr
+     "i#{group_letter}#{group_number}#{student_number}"
+    end
   end
 end
