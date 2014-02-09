cd64ad11afc602a9d8f953e324ef07da97879a44
obliviusm <obliviusm@gmail.com>
2014-02-09 16:04:17 +0200

    controller tests for user managing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,5 @@ RSpec.configure do |config|
 
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, :type => :controller
+  config.extend ControllerMacros, :type => :controller
 end
