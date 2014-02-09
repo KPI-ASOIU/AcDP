b657eb7592fb4594adf4d2d3cf696b4a6fa25fbf
Dmytro Yarmak <dyarmak@heliostech.hk>
2014-02-09 12:35:25 +0200

    Add methods to manage roles to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,37 @@ class User < ActiveRecord::Base
   validates_presence_of   :login
   validates_uniqueness_of :login
 
+  validates_format_of     :email, :with  => Devise.email_regexp, :allow_blank => true, :if => :email_changed?
+
   validates_length_of       :password, within: Devise.password_length, :if => :password_required?
   validates_presence_of     :password, :if => :password_required?
   validates_confirmation_of :password, :if => :password_required?
-  validates_format_of     :email, :with  => Devise.email_regexp, :allow_blank => true, :if => :email_changed?
 
   def password_required?
     !persisted? || !password.nil? || !password_confirmation.nil?
+  end
+
+  ROLES = %w[student worker teacher admin]
+
+  def roles=(roles)
+    self.role = (roles & ROLES).map { |r| 2**ROLES.index(r) }.inject(0, :+)
+  end
+
+  def add_role(role)
+    self.roles = roles + [role]
+  end
+
+  def remove_role(role)
+    self.roles = roles - [role]
+  end
+
+  def has_role?(role)
+    roles.include?(role.to_s)
+  end
+
+  def roles
+    ROLES.reject do |r|
+      ((role.to_i || 0) & 2**ROLES.index(r)).zero?
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -102,4 +102,244 @@ describe User do
       end
     end
   end
+
+  describe "#roles=" do
+    before(:each) { subject.role = 0 }
+
+    context "when passed empty array" do
+      before(:each) { subject.roles = [] }
+      its(:role) { should eq(0) }
+    end
+
+    context "when passed unknown role" do
+      before(:each) { subject.roles = ['unknown'] }
+      its(:role) { should eq(0) }
+    end
+
+    context "when passed ['student']" do
+      before(:each) { subject.roles = ["student"] }
+      its(:role) { should eq(1) }
+    end
+
+    context "when passed ['worker']" do
+      before(:each) { subject.roles = ["worker"] }
+      its(:role) { should eq(2) }
+    end
+
+    context "when passed ['teacher']" do
+      before(:each) { subject.roles = ["teacher"] }
+      its(:role) { should eq(4) }
+    end
+
+    context "when passed ['admin']" do
+      before(:each) { subject.roles = ["admin"] }
+      its(:role) { should eq(8) }
+    end
+
+    context "when passed ['admin', 'teacher']" do
+      before(:each) { subject.roles = ["admin", "teacher"] }
+      its(:role) { should eq(12) }
+    end
+
+    context "when passed ['teacher', 'admin', 'worker']" do
+      before(:each) { subject.roles = ["admin", "teacher", "worker"] }
+      its(:role) { should eq(14) }
+    end
+  end
+
+  describe "#roles" do
+    context "when role is 0" do
+      before(:each) { subject.role = 0 }
+      its(:roles) { should match_array([]) }
+    end
+
+    context "when role is 1" do
+      before(:each) { subject.role = 1 }
+      its(:roles) { should match_array(["student"]) }
+    end
+
+    context "when role is 2" do
+      before(:each) { subject.role = 2 }
+      its(:roles) { should match_array(["worker"]) }
+    end
+
+    context "when role is 4" do
+      before(:each) { subject.role = 4 }
+      its(:roles) { should match_array(["teacher"]) }
+    end
+
+    context "when role is 8" do
+      before(:each) { subject.role = 8 }
+      its(:roles) { should match_array(["admin"]) }
+    end
+
+    context "when role is 12" do
+      before(:each) { subject.role = 12 }
+      its(:roles) { should match_array(["admin", "teacher"]) }
+    end
+
+    context "when role is 14" do
+      before(:each) { subject.role = 14 }
+      its(:roles) { should match_array(["admin", "teacher", "worker"]) }
+    end
+  end
+
+  describe "#has_role?" do
+    context "user has role 10" do
+      before(:each) { subject.role = 10 }
+      it "should not be unknown" do
+        expect(subject.has_role? "unknown").to be_false
+      end
+      it "should not be student" do
+        expect(subject.has_role? "student").to be_false
+      end
+      it "should be worker" do
+        expect(subject.has_role? "worker").to be_true
+      end
+      it "should not be teacher" do
+        expect(subject.has_role? "teacher").to be_false
+      end
+      it "should be admin" do
+        expect(subject.has_role? "admin").to be_true
+      end
+    end
+  end
+
+  describe "#add_role" do
+    context "when role is 0" do
+      before(:each) { subject.role = 0 }
+
+      context "when add_role 'unknown'" do
+        it "should not change role" do
+          expect(lambda {
+            subject.add_role "unknown"
+          }).to_not change(subject, :role)
+        end
+      end
+
+      context "when add_role 'student'" do
+        it "should not change role" do
+          expect(lambda {
+            subject.add_role "student"
+          }).to change(subject, :role).by(1)
+        end
+      end
+
+      context "when add_role 'worker'" do
+        it "should not change role" do
+          expect(lambda {
+            subject.add_role "worker"
+          }).to change(subject, :role).by(2)
+        end
+      end
+
+      context "when add_role 'teacher'" do
+        it "should not change role" do
+          expect(lambda {
+            subject.add_role "teacher"
+          }).to change(subject, :role).by(4)
+        end
+      end
+
+
+      context "when add_role 'admin'" do
+        it "should not change role" do
+          expect(lambda {
+            subject.add_role "admin"
+          }).to change(subject, :role).by(8)
+        end
+      end
+    end
+
+    context "when role is 10" do
+      before(:each) { subject.role = 10 }
+
+      context "when add_role 'unknown'" do
+        it "should not change role" do
+          expect(lambda {
+            subject.add_role "unknown"
+          }).to_not change(subject, :role)
+        end
+      end
+
+      context "when add_role 'student'" do
+        it "should not change role" do
+          expect(lambda {
+            subject.add_role "student"
+          }).to change(subject, :role).by(1)
+        end
+      end
+
+      context "when add_role 'worker'" do
+        it "should not change role" do
+          expect(lambda {
+            subject.add_role "worker"
+          }).to_not change(subject, :role).by(2)
+        end
+      end
+
+      context "when add_role 'teacher'" do
+        it "should not change role" do
+          expect(lambda {
+            subject.add_role "teacher"
+          }).to change(subject, :role).by(4)
+        end
+      end
+
+
+      context "when add_role 'admin'" do
+        it "should not change role" do
+          expect(lambda {
+            subject.add_role "admin"
+          }).to_not change(subject, :role).by(8)
+        end
+      end
+    end
+  end
+  describe "#remove_role" do
+    context "when role is 10" do
+      before(:each) { subject.role = 10 }
+
+      context "when remove_role 'unknown'" do
+        it "should not change role" do
+          expect(lambda {
+            subject.remove_role "unknown"
+          }).to_not change(subject, :role)
+        end
+      end
+
+      context "when remove_role 'student'" do
+        it "should not change role" do
+          expect(lambda {
+            subject.remove_role "student"
+          }).to_not change(subject, :role)
+        end
+      end
+
+      context "when remove_role 'worker'" do
+        it "should not change role" do
+          expect(lambda {
+            subject.remove_role "worker"
+          }).to change(subject, :role).by(-2)
+        end
+      end
+
+      context "when remove_role 'teacher'" do
+        it "should not change role" do
+          expect(lambda {
+            subject.remove_role "teacher"
+          }).to_not change(subject, :role)
+        end
+      end
+
+
+      context "when remove_role 'admin'" do
+        it "should not change role" do
+          expect(lambda {
+            subject.remove_role "admin"
+          }).to change(subject, :role).by(-8)
+        end
+      end
+    end
+  end
 end
