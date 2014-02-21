f44cc9841521122afbff757464ce6da864163359
Dmytro Yarmak <dyarmak@heliostech.hk>
2014-02-21 11:10:30 +0200

    Fix problem with db:create while deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,7 +55,7 @@ namespace :deploy do
       sudo 'start nginx'
 
       within release_path do
-        with rails_env: fetch(:rails_env) do
+        with rails_env: 'production' do
           execute :rake, "db:create"
         end
       end
