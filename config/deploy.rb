710a732a6339e7a38b6939560fb2e31d77c2a32c
Dmytro Yarmak <dyarmak@heliostech.hk>
2014-02-22 23:14:35 +0200

    Fix problim with deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,6 +66,7 @@ namespace :deploy do
   task :symlink do
     on roles(:all) do
       execute "ln -s #{shared_path}/Procfile #{release_path}/Procfile"
+      execute "rm -rf #{release_path}/public/system"
       execute "ln -s #{shared_path}/system #{release_path}/public/system"
     end
   end
