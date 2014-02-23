9e6ef0d4bd1517635edc23a8afa40f51a270ecb4
Dmitriy <dkalpackchi@gmail.com>
2014-02-23 19:16:05 +0200

    Unread messages count update

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,10 @@
 class Message < ActiveRecord::Base
+	after_create do |message|
+		message.subscriptions.reject(message.author).each { |m| 
+			Subscription.increment_counter(:unread_messages_count, m.id)
+		}
+	end
+
 	belongs_to :author, class_name: "User"
 	belongs_to :conversation
 	has_many :subscriptions, through: :conversation
