acf68a17abdf5cfc905402e52499997cded873d9
Dmitriy <dkalpackchi@gmail.com>
2014-02-27 20:28:25 +0200

    Tests fixing for unread messages counter

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -16,18 +16,21 @@ describe Message do
   describe '#find_all_subscription_ids_except' do
     it 'should return the array of proper ids' do
       message = FactoryGirl.create(:message, :with_subscriptions)
-      expect(lambda {
-        message.send(:find_all_subscription_ids_except).with(message.author_id)
-      }).to include(message.subscriptions.pluck(:id).reject{ |id| id == message.author_id })
+      expect(
+        message.send(:find_all_subscription_ids_except, message.author_id)
+      ).to match_array(message.subscriptions.pluck(:id).reject{ |id| id == message.author_id })
     end
   end
 
   describe '#inc_unread_messages' do
     it 'should increment unread messages counter of each subscription in conversation except author\'s one' do
       message = FactoryGirl.create(:message, :with_subscriptions)
-      expect(lambda {
-        message.send(:inc_unread_messages) 
-      }).to message.subscriptions.each { |sub| change{sub}.by(1) }
+      sub_ids = message.subscriptions.pluck(:unread_messages_count)
+      expect(message).to receive(:find_all_subscription_ids_except).with(message.author_id)
+      message.send(:inc_unread_messages) 
+      expect(
+        message.subscriptions.pluck(:unread_messages_count)
+      ).to eq(sub_ids.each { |id| id += 1 })
     end
   end
 
