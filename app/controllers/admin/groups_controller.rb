95f35480e5b57a5266d9126bda4f29089b719c8a
Ruslan <rdanilchuk@gmail.com>
2014-03-16 04:00:17 +0200

    group management

---
+++ app/controllers/admin/groups_controller.rb
@@ -0,0 +1,49 @@
+class Admin::GroupsController < ApplicationController
+	before_action :set_group, only: [:show, :edit, :destroy, :update]
+
+	def index
+		@groups = Group.order("name").page(params[:page])
+	end
+
+	def edit
+	end
+
+	def show
+	end
+
+	def update
+		if @group.update(group_params)
+	      redirect_to admin_groups_path, notice: t('groups.notice.updated')
+	    else
+	      render action: 'edit'
+	    end
+	end
+
+	def create
+		@group = Group.new(group_params)
+	    if @group.save
+	      redirect_to admin_groups_path, notice: t('groups.notice.created')
+	    else
+	      render action: 'new'
+	    end
+	end
+
+	def new
+		@group = Group.new
+	end
+
+	def destroy
+		@group.destroy
+		redirect_to admin_groups_path
+	end
+
+	def set_group
+    	@group = Group.find(params[:id])
+  	end
+
+  	private
+  	def group_params
+    params.require(:group)
+       .permit(:name, :start_year, :graduation_year, :speciality, :speciality_code, :full_time, :degree)
+  end
+end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,18 @@
 class Group < ActiveRecord::Base
-	validates :name, :start_year, :graduation_year, :speciality, :speciality_code, :full_time, :degree, presence: true
+	validates :name, :start_year, :graduation_year, :speciality, :speciality_code, :degree, presence: true
 	validates :start_year, numericality: {greater_than: 0}
 	validates :graduation_year, numericality: {greater_than: :start_year}
+  validates :full_time, :inclusion => {:in => [true, false]} #dont touch this variable
+
+	DEGREE = %w[bachelor specialist master]
+
+	FORMS = %w[intramural extramural]
+
+	def degrees
+		degree.nil? ? 1 : degree
+	end
+
+	def forms
+		full_time.nil? ? true : full_time
+	end
 end
