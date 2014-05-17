9628557113b47d06eaad3afb5f822a6a00604f69
Ruslan <rdanilchuk@gmail.com>
2014-05-17 21:40:18 +0300

    calendar changes

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,6 +1,6 @@
 class CalendarController < ApplicationController
   def calendar
   	@date = params[:date] ? Date.parse(params[:date]) : Date.today
-  	@tasks_by_date = current_user.executing_tasks.group_by{ |item| item.end_date.to_date }
+  	@tasks_by_date = (current_user.executing_tasks & current_user.leading_tasks).group_by{ |item| item.end_date ? item.end_date.to_date : nil }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,22 @@
 AcDP::Application.routes.draw do
-  get "calendar/index"
   get 'documents', to: 'documents#index'
   get 'documents/shared', to: 'documents#shared'
   get 'documents/shared/:user_id', to: 'documents#shared', as: 'document_shared_root'
   get 'documents/shared/:user_id/:id', to: 'documents#shared', as: 'document_shared'
   get 'documents/:id', to: 'documents#index', as: 'document'
   post 'documents', to: 'documents#new'
-<<<<<<< HEAD
-<<<<<<< HEAD
   post 'documents/update', to: 'documents#update'
   post 'documents/update_access', to: 'documents#update_access'
   post 'documents/:id', to: 'documents#new', as: 'document_new'
   delete 'documents/delete/:delete_id', to: 'documents#delete', as: 'document_delete_root'
   delete 'documents/:id/delete/:delete_id', to: 'documents#delete', as: 'document_delete'
-=======
   get 'calendar', to: 'calendar'
   delete 'documents', to: 'documents#delete'
->>>>>>> Create calendar template
-=======
   get 'calendar', to: 'calendar'
   delete 'documents', to: 'documents#delete'
-=======
   post 'documents/update', to: 'documents#update'
   post 'documents/update_access', to: 'documents#update_access'
-  post 'documents/:id', to: 'documents#new', as: 'document_new'
-  delete 'documents/delete/:delete_id', to: 'documents#delete', as: 'document_delete_root'
-  delete 'documents/:id/delete/:delete_id', to: 'documents#delete', as: 'document_delete'
->>>>>>> Added basic documents functionality
->>>>>>> Added basic documents functionality
+  post 'documents/:id', to: 'documents#new'
 
   root "users#show_current"
 
