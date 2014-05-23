5ff181697664390b995e6ee1bc1a5ce7b0795f9f
Troilk <troilk@hotmail.com>
2014-05-23 23:16:35 +0300

    Fixed ui_manager.js

--- app/assets/javascripts/ui_manager.js
+++ app/assets/javascripts/ui_manager.js
@@ -25,6 +25,12 @@ $(document).ready(function(){
     }
   });
 
+  $(".j-admin-search-type-combo").on("click", "a", function(e){
+      var $target = $(this);
+      $(".j-admin-search-btn-text").text($target.text());
+      $(".j-admin-search-type-input").val($target.data("search-type"));
+  });
+
   $("tr[data-link]").on("click", function(e) {
     e.stopPropagation();
     window.location = this.getAttribute("data-link");
