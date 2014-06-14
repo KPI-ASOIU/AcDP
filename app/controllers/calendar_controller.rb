fb224d7f4d07bb3fad576c1e5f81132bfd50b151
obliviusm <obliviusm@gmail.com>
2014-06-14 19:03:05 -0400

    show task in calendar somehow

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,6 +1,14 @@
 class CalendarController < ApplicationController
+  before_action :set_current_user
+
   def calendar
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
-    @tasks_by_date = (current_user.executing_tasks | current_user.leading_tasks).group_by{ |item| item.end_date ? item.end_date.to_date : nil }
+    @tasks_by_date = Task.connected_to_me.group_by{ |item| item.end_date ? item.end_date.to_date : nil }
+  end
+
+  def day
+    date_start = Time.parse(params[:date]) #midnight of date
+    date_finish = date_start + 1.day
+    @tasks = Task.where{(end_date >= date_start) & (end_date <= date_finish)}
   end
 end
