b89fcca5a213905e17977228e84563810249fd0d
obliviusm <obliviusm@gmail.com>
2014-06-14 20:06:00 -0400

    add time to tasks calendar table

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -4,11 +4,12 @@ class CalendarController < ApplicationController
   def calendar
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
     @tasks_by_date = Task.connected_to_me.group_by{ |item| item.end_date ? item.end_date.to_date : nil }
+    @events_by_date = Event.connected_to_me.group_by{ |item| item.date ? item.date.to_date : nil }
   end
 
   def day
     date_start = Time.parse(params[:date]) #midnight of date
     date_finish = date_start + 1.day
-    @tasks = Task.where{(end_date >= date_start) & (end_date <= date_finish)}
+    @tasks = Task.connected_to_me.where{(end_date >= date_start) & (end_date <= date_finish)}
   end
 end
