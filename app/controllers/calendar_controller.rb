6241f328dedfff7a4c8a37a8c75923edc5383d0d
obliviusm <obliviusm@gmail.com>
2014-06-17 18:50:48 -0400

    not show tasks in calendar for not department worker

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -3,7 +3,9 @@ class CalendarController < ApplicationController
 
   def calendar
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
-    @tasks_by_date = Task.connected_to_me.group_by{ |item| item.end_date ? item.end_date.to_date : nil }
+    if can? :crud, Task
+      @tasks_by_date = Task.connected_to_me.group_by{ |item| item.end_date ? item.end_date.to_date : nil }
+    end
     @events_by_date = Event.connected_to_me.group_by{ |item| item.date ? item.date.to_date : nil }
   end
 
@@ -11,7 +13,9 @@ class CalendarController < ApplicationController
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
     date_start = Time.parse(params[:date]) #midnight of date
     date_finish = date_start + 1.day
-    @tasks = Task.connected_to_me.where{(end_date >= date_start) & (end_date <= date_finish)}
+    if can? :crud, Task
+      @tasks = Task.connected_to_me.where{(end_date >= date_start) & (end_date <= date_finish)}
+    end
     @events = Event.connected_to_me.where{(date >= date_start) & (date <= date_finish)}
   end
 end
