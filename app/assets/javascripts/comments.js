aa39a913929fce29bf420d4825524ef93c99016a
Dmytro Yarmak <dyarmak@heliostech.hk>
2014-06-19 00:52:33 +0300

    Enable standalone version of WS

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,14 +1,14 @@
 function newComment(comment) {
-	jQuery('#no_comments').fadeOut()
-	jQuery('#comments .jspPane').prepend(comment.html)
-	$("time[data-time-ago]").timeago()
-	$('#comment_' + comment.id).hide().fadeIn('slow')
-	$('.new-comment').val('')
+  jQuery('#no_comments').fadeOut()
+  jQuery('#comments .jspPane').prepend(comment.html)
+  $("time[data-time-ago]").timeago()
+  $('#comment_' + comment.id).hide().fadeIn('slow')
+  $('.new-comment').val('')
 }
 
 $(document).ready(function(){
-	window.dispatcher = new WebSocketRails(window.location.host + '/websocket');
-	(function(){
-		dispatcher.bind('comments.new_comment', newComment);
-	})();
+  window.dispatcher = new WebSocketRails(window.location.host + ':3001/websocket');
+  (function(){
+    dispatcher.bind('comments.new_comment', newComment);
+  })();
 });
