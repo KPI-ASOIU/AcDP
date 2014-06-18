520870c64998a2320e8f09cb4cc15c913e23bae2
Dmytro Yarmak <dyarmak@heliostech.hk>
2014-06-18 21:53:45 +0300

    Update deploy config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{deployer@77.47.194.148}
-role :web, %w{deployer@77.47.194.148}
-role :db,  %w{deployer@77.47.194.148}
+role :app, %w{deployer@188.226.191.170}
+role :web, %w{deployer@188.226.191.170}
+role :db,  %w{deployer@188.226.191.170}
 
 # Extended Server Syntax
 # ======================
@@ -14,7 +14,7 @@ role :db,  %w{deployer@77.47.194.148}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '77.47.194.148', user: 'deployer', roles: %w{web app db}
+server '188.226.191.170', user: 'deployer', roles: %w{web app db}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
